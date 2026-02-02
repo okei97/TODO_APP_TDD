a3685c2cebf8e9d69dc5d60a38414484f2566b9b
okei97 <kyuurisan.1997@gmail.com>
2026-02-02 21:12:44 +0900

    fix import pass of create_todo.py

--- backend/app/usecase/create_todo.py
+++ backend/app/usecase/create_todo.py
@@ -1,5 +1,5 @@
 from app.domain.todo import Todo
-from backend.app.infrastructure.todo_repository import save
+from app.infrastructure.todo_repository import save
 
 def create_todo(title: str) -> Todo:
     todo = Todo(title)
