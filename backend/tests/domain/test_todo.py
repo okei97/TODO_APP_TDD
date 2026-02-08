2d81d4fa4694611468c510cf82cf281888f6ec19
okei97 <kyuurisan.1997@gmail.com>
2026-02-08 15:37:17 +0900

    add boundary value tests

--- backend/tests/domain/test_todo.py
+++ backend/tests/domain/test_todo.py
@@ -8,3 +8,13 @@ def test_todo_empty_title():
 def test_todo_initial_completed_false():
     todo = Todo("test")
     assert todo.completed is False
+
+# titleが1文字の場合のテストを追加
+def test_todo_title_one_character():
+    todo = Todo("A")
+    assert todo.title == "A"
+
+def test_todo_title_max_length():
+    max_length_title = "A" * 100
+    todo = Todo(max_length_title)
+    assert todo.title == max_length_title
