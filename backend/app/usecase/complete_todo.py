b1b52fa55cd211ad4c36aec224b3d90431287d22
okei97 <kyuurisan.1997@gmail.com>
2026-02-22 22:28:17 +0900

    usecase層改修

--- backend/app/usecase/complete_todo.py
+++ backend/app/usecase/complete_todo.py
@@ -1,8 +1,9 @@
 from app.domain.todo import Todo
 from app.infrastructure.todo_repository import get,save
+from datetime import datetime
 
-def complete_todo(id: str) -> Todo:
+def complete_todo(id: str, now: datetime) -> Todo:
     todo = get(id)
-    todo.complete()
+    todo.complete(now)
     save(todo)
     return todo
